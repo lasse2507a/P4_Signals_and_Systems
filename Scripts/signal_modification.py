ae0f6d21d3d80eaf1c3562c0656c8732d36faa3c
lasse2507a <44569984+lasse2507a@users.noreply.github.com>
2020-03-21 23:33:15 +0100

    Create signal_modification.py

---
+++ Scripts/signal_modification.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 23:22:09 2020
+
+@author: lasse
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# =============================================================================
+# Synthetic Data Generation
+# =============================================================================
+def sinew(J = 18, freq = 1000, phase = 0):
+    """
+    Signal consisting of a single sine wave with specified 
+    frequency, phase, and amount of points.
+    """
+    N = 2**J
+    t = np.arange(1 , N+1)
+    A = 2 * np.pi * t / N
+    x = np.sin(A * freq + phase)
+    return x
+
+
+def fsinew(J = 18, freq1 = 13, freq2 = 20, freq3 = 40, freq4 = 1000, phase1 = 0, 
+                   phase2 = 0, phase3 = 0, phase4 = 0, phase5 = 0):
+    """
+    Signal consisting of four sine waves.
+    """
+    N = 2**J
+    t = np.arange(1, N+1)
+    A = 2 * np.pi * t / N
+    x1 = np.sin(A * freq1 + phase1)
+    x2 = np.sin(A * freq2 + phase2)
+    x3 = np.sin(A * freq3 + phase3)
+    x4 = np.sin(A * freq4 + phase4)
+    x_sum = x1 + x2 + x3 + x4
+    return x_sum
+
+
+# =============================================================================
+# Signal Modification Functions
+# =============================================================================
+
+
+# =============================================================================
+# Application of Signal Modifications
+# =============================================================================
+
+
+# =============================================================================
+# Execution and Plotting
+# =============================================================================
