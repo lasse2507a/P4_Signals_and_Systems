983a9a9f97aeffe848051eb98652180c068ee3ff
jmoerk123 <jmoerk0101@gmail.com>
2020-05-01 12:59:18 +0200

    Update sound_import_and_transform.py

--- Scripts/sound_import_and_transform.py
+++ Scripts/sound_import_and_transform.py
@@ -8,22 +8,33 @@ Created on Tue Apr 21 13:40:55 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
+from scipy.io import wavfile
 
 #Navn paa lydklip
 filename = "sound/jacob_snak.wav"
 
 #Uddrager samplingsfrekvens og data fra lydfil
-fs, data1 = sp.io.wavfile.read(filename)
+fs, data1 = wavfile.read(filename)
 time = len(data1)/fs
 highest = np.amax(data1)
 data = data1/highest
+
+#FIR filter by windowing
+fir = sp.signal.firwin(5, 4400, fs = fs)
+
 
 #Downsampling
 down = 5
 signal = sp.signal.decimate(data, down, ftype='fir')
 
+#Some windows (find more: https://bit.ly/2KxlMsO)
+M = int(20e-3*fs)         #number of samples
+hamming = sp.signal.windows.hamming(M)
+boxcar = sp.signal.windows.boxcar(M)
+flattop = sp.signal.windows.flattop(M)
+
 #Short time Fourier transform
-f, t, STFT  = sp.signal.stft(signal, fs/down, nperseg=256)
+f, t, STFT  = sp.signal.stft(signal, fs/down, window = 'hamming', nperseg=882)
 
 A = STFT
 
@@ -39,7 +50,7 @@ T, ISTFT  = sp.signal.istft(STFT, fs/down, nperseg=256)
 ISTFT_del = ISTFT[(ISTFT >= 1) & (ISTFT <= -1)]
 
 B = ISTFT
-sp.io.wavfile.write('modi_sound/jacob_snak_down_stft.wav', int(fs/down), ISTFT)
+#sp.io.wavfile.write('modi_sound/jacob_snak_down_stft.wav', int(fs/down), ISTFT)
 
 
 
