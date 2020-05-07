72def5ce3e92ccec85f4197b16be7470d32e0fe8
jmoerk123 <jmoerk0101@gmail.com>
2020-05-07 11:46:40 +0200

    Update filter_by_fsm.py

--- Miniprojekt/filter_by_fsm.py
+++ Miniprojekt/filter_by_fsm.py
@@ -34,24 +34,37 @@ def fsinew(J = 18, fs = 2**12 , freq1 = 200, freq2 = 400, freq3 = 500, freq4 = 8
 
 def H_ideal(fs = 8000, cutoff = 1000):
     ones = np.ones(cutoff)
-    zeros = np.zeros(fs-cutoff)
-    H_ideal = np.concatenate((ones, zeros))
+    zeros = np.zeros(fs-2*cutoff)
+    H_ideal = np.concatenate((ones, zeros, ones))
     return H_ideal
 
 
 def H(fs = 8000, cutoff = 1000, a = 1000):
     def f(x):
-        return -(1)*x + 1
+        return -1*x + 1
+    def g(x):
+        return 1*x
     x = np.linspace(0,1, a)
     y = f(x)
+    y2 = g(x)
     ones = np.ones(cutoff)
-    zeros = np.zeros(fs-cutoff-len(x))
-    H = np.concatenate((ones, y, zeros))  
+    zeros = np.zeros(fs-2*cutoff-2*len(x))
+    H = np.concatenate((ones, y, zeros, y2, ones))  
     return H
 
 
 
 def H_sampled(H_ideal, N):
+    T = int(len(H_ideal)/(N))
+    H_sampled = np.zeros(N)
+    x = np.zeros(N)
+    H_sampled[0] = H_ideal[0]
+    for i in range(1, N):
+        H_sampled[i] = H_ideal[i*T]
+        x[i] = i*T
+    return H_sampled, x
+
+def H_sampled2(H_ideal, N):
     T = int(len(H_ideal)/(N))
     H_sampled = np.zeros(N)
     x = np.zeros(N)
@@ -100,11 +113,24 @@ def plt_magnitude_dB(H_pad):
 # Using functions
 # =============================================================================
     
+points = 20
 
-H_omega = H_ideal(cutoff = 1000)
-H_k, x = H_sampled(H_omega, 20)
-h = h(H_k)
-H_pad = zeropad_fft(h)
+H_omega0 = H_ideal()
+H_omega1 = H(a = 500)
+H_omega2 = H(a = 1000)
+H_omega3 = H(a = 1500)
+H_k0, x = H_sampled(H_omega0, points)
+H_k1, x = H_sampled(H_omega1, points)
+H_k2, x = H_sampled(H_omega2, points)
+H_k3, x = H_sampled(H_omega3, points)
+h0 = h(H_k0)
+h1 = h(H_k1)
+h2 = h(H_k2)
+h3 = h(H_k3)
+H_pad0 = zeropad_fft(h0)
+H_pad1 = zeropad_fft(h1)
+H_pad2 = zeropad_fft(h2)
+H_pad3 = zeropad_fft(h3)
 
 
 # =============================================================================
@@ -114,17 +140,26 @@ H_pad = zeropad_fft(h)
 # H(omega) and H(k)
 plt.figure(figsize=(10,5))
 plt.title('Ideal lowpass filter', fontsize = 20)
-plt.plot(H_omega, label ='$|H(\omega)$|')
-plt.plot(x, H_k, '*', label = '$|H(k)|$')
+plt.plot(H_omega0, label ='$|H(\omega)$|')
+plt.plot(x, H_k0, '*', label = '$|H(k)|$')
+plt.plot(H_omega1, label ='$|H(\omega)$|')
+plt.plot(x, H_k1, '*', label = '$|H(k)|$')
+plt.plot(H_omega2, label ='$|H(\omega)$|')
+plt.plot(x, H_k2, '*', label = '$|H(k)|$')
+plt.plot(H_omega3, label ='$|H(\omega)$|')
+plt.plot(x, H_k3, '*', label = '$|H(k)|$')
 plt.xlabel('Frequency [Hz]')
 plt.legend()
 plt.grid()
-plt.savefig('figure/ideal_sam.pdf')
+#   plt.savefig('figure/ideal_sam.pdf')
 
 # Impulse response
 plt.figure(figsize = (10,5))
-plt.title('Impulse response')
-plt.plot(h, '*', label = 'Imulse response')
+plt.title('Impulse response', fontsize = 20)
+plt.plot(h0, '*', label = 'Imulse response')
+plt.plot(h1, '*', label = 'Imulse response')
+plt.plot(h2, '*', label = 'Imulse response')
+plt.plot(h3, '*', label = 'Imulse response')
 plt.xlabel('Sample number')
 plt.ylabel('Amplitude')
 plt.legend()
@@ -133,8 +168,11 @@ plt.grid()
 
 # Magnitude
 plt.figure(figsize = (10,5))
-plt.title('Magnitude')
-plt.plot(np.linspace(0, 4000, len(H_pad)), H_pad, label = 'Magnitude')
+plt.title('Magnitude', fontsize = 20)
+plt.plot(np.linspace(0, 4000, len(H_pad0)), H_pad0, label = 'Magnitude')
+plt.plot(np.linspace(0, 4000, len(H_pad1)), H_pad1, label = 'Magnitude')
+plt.plot(np.linspace(0, 4000, len(H_pad2)), H_pad2, label = 'Magnitude')
+plt.plot(np.linspace(0, 4000, len(H_pad3)), H_pad3, label = 'Magnitude')
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('Magnitude')
 plt.plot(1000, 1/np.sqrt(2), '*')
@@ -144,8 +182,11 @@ plt.grid()
 
 # Frequency response
 plt.figure(figsize = (10,5))
-plt.title('Frequency response')
-plt.plot(np.linspace(0, 4000, len(H_pad)), 20*np.log10(H_pad), label = 'Frequency response $|H(f)|$')
+plt.title('Frequency response', fontsize = 20)
+plt.plot(np.linspace(0, 4000, len(H_pad0)), 20*np.log10(H_pad0), label = 'Frequency response $|H(f)|$')
+plt.plot(np.linspace(0, 4000, len(H_pad1)), 20*np.log10(H_pad1), label = 'Frequency response $|H(f)|$')
+plt.plot(np.linspace(0, 4000, len(H_pad2)), 20*np.log10(H_pad2), label = 'Frequency response $|H(f)|$')
+plt.plot(np.linspace(0, 4000, len(H_pad3)), 20*np.log10(H_pad3), label = 'Frequency response $|H(f)|$')
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('$|H(f)|$ [dB]')
 plt.ylim(-100, 10)
